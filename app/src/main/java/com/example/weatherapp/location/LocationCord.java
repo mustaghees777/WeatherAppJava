ecfce5995bfe96534d087bb6a6e1eec40b446ec8
Mustaghees Abbasi <mustagheesabbasi@gmail.com>
2024-10-01 12:31:50 +0500

    WeatherApp

---
+++ app/src/main/java/com/example/weatherapp/location/LocationCord.java
@@ -0,0 +1,9 @@
+package com.example.weatherapp.location;
+
+public class LocationCord {
+    public static String lat = "";
+    public static String lon = "";
+        public final static String API_KEY = "7c040d85f6a8c2cfb8c3c9f81fb07154";
+//    public final static String API_KEY = "eeb8b40367eee691683e5a079e2fa695";
+}
+
